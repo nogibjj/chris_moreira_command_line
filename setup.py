fc5e167c205570d06a8d86e1ac342693d913acfd
Christian Moreira <chris.moreira@duke.edu>
2024-10-23 20:58:43 -0400

    .

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-# Setup for packaging and installing the DatabricksPipeline project
+
 setup(
     name="DatabricksPipeline",
     version="0.1.0",
-    description="DatabricksPipeline for ETL and querying",
+    description="DatabricksPipeline",
     author="Christian Moreira",
     author_email="chris.moreira@duke.edu",
     packages=find_packages(),
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "etl_query=main:main",  # This allows `etl_query` to run your main() function in main.py
+            "etl_query=main:main",
         ],
     },
 )
